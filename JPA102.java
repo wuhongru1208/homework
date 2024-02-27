24eccd6b321a318683595779fb3784be2a4a082b
wuhongru1208 <161403493+wuhongru1208@users.noreply.github.com>
2024-02-27 11:00:44 +0800

    Add files via upload

---
+++ JPA102.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class JPA102 {
+
+    public static void main(String[] args) {
+    System.out.println("Please input");
+    Scanner scn=new Scanner(System.in);
+    double k=scn.nextDouble();
+    System.out.println(k+" kg="+(k*2.20462)+" ponds");
+
+    }
+}
